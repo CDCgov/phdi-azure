d9fcc6d3a2d228cdbc66327a1cbf723f4d98cb05
Nick Clyde <nick@clyde.tech>
2022-09-27 12:11:19 -0700

    Update workflows

--- terraform/implementation/backend.tf
+++ terraform/implementation/backend.tf
@@ -7,17 +7,13 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
-    storage_account_name = "abcd1234"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-    use_msi              = true
-    subscription_id      = "00000000-0000-0000-0000-000000000000"
-    tenant_id            = "00000000-0000-0000-0000-000000000000"
+    container_name = "tfstate"
+    key            = "prod.terraform.tfstate"
+    use_msi        = true
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id            = var.subscription_id
+  subscription_id = var.subscription_id
 }
