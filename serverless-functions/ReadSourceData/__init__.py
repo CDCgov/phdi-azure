1ae86f4ce9c1fb1f58af7b2efa84eb3b9a260f7d
Nick Clyde <nick@clyde.tech>
2022-10-25 12:00:17 -0700

    Aha! blob.name includes container name

--- serverless-functions/ReadSourceData/__init__.py
+++ serverless-functions/ReadSourceData/__init__.py
@@ -20,21 +20,23 @@ def main(blob: func.InputStream, queue: func.Out[str]) -> None:
         outcome and associated HTTP status code.
     """
     logging.debug("File uploaded, reading source data...")
-    logging.info("Python Blob trigger function processed %s", blob.name)
 
     try:
         # Determine data type and root template.
-        if blob.folder == "elr":
-            message_type = "hl7v2"
-            root_template = "ORU_R01"
+        filename_parts = blob.name.split("/")
+        if filename_parts[0] == "source-data":
 
-        elif blob.folder == "vxu":
-            message_type = "hl7v2"
-            root_template = "VXU_V04"
+            if filename_parts[1] == "elr":
+                message_type = "hl7v2"
+                root_template = "ORU_R01"
 
-        elif blob.folder == "ecr":
-            message_type = "ccda"
-            root_template = "CCD"
+            elif filename_parts[1] == "vxu":
+                message_type = "hl7v2"
+                root_template = "VXU_V04"
+
+            elif filename_parts[1] == "ecr":
+                message_type = "ccda"
+                root_template = "CCD"
 
         else:
             raise Exception("Invalid file type.")
@@ -53,7 +55,7 @@ def main(blob: func.InputStream, queue: func.Out[str]) -> None:
                 "message": message,
                 "message_type": message_type,
                 "root_template": root_template,
-                "filename": f"source-data/{blob.folder}/{blob.name}.{blob.extension}",
+                "filename": blob.name,
             }
 
             queue_message = json.dumps(queue_message).encode("utf-8")
