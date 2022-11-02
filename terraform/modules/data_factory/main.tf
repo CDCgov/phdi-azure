9c1a0205f5df9439442a33fb9a76bd3b40022ce2
DanPaseltiner <dan@skylight.digital>
2022-11-02 09:54:40 -0400

    Attempt reading parameters from JSON config.

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -21,16 +21,14 @@ resource "azurerm_data_factory" "phdi_data_factory" {
   }
 }
 
+locals {
+  ingestion-pipeline-config = jsondecode(file("../modules/data_factory/ingestion-pipeline.json"))
+}
+
 resource "azurerm_data_factory_pipeline" "phdi_ingestion" {
   name            = "phdi-ingestion"
   data_factory_id = azurerm_data_factory.phdi_data_factory.id
   concurrency     = 10 // Max concurrent instances of the pipeline, between 1 and 50. May need to tune this in the future. 
-  parameters = {
-    "filename" : "",
-    "message" : "",
-    "message_type" : "",
-    "root_template" : "",
-  }
-
-  activities_json = jsonencode(jsondecode(file("../modules/data_factory/ingestion-pipeline.json")).properties.activities)
+  parameters = jsonencode(local.ingestion-pipeline-config.properties.parameters)
+  activities_json = jsonencode(local.ingestion-pipeline-config.properties.activities)
 }
