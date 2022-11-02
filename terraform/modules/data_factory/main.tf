b02fe70ed5bea57b45781d6a3c6f166750ee0439
DanPaseltiner <dan@skylight.digital>
2022-11-01 22:07:00 -0400

    format Terraform

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -22,9 +22,9 @@ resource "azurerm_data_factory" "phdi_data_factory" {
 }
 
 resource "azurerm_data_factory_pipeline" "ingestion" {
-  name                = "ingestion"
-  data_factory_id     = azurerm_data_factory.phdi_data_factory.id
-  concurrency         = 10 // Max concurrent instances of the pipeline, between 1 and 50. May need to tune this in the future. 
+  name            = "ingestion"
+  data_factory_id = azurerm_data_factory.phdi_data_factory.id
+  concurrency     = 10 // Max concurrent instances of the pipeline, between 1 and 50. May need to tune this in the future. 
   parameters = {
     "filename" : "",
     "message" : "",
