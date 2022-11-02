b495f6ca213567fab8502141f9975495aef3365a
DanPaseltiner <dan@skylight.digital>
2022-11-02 14:32:26 -0400

    Format Terraform

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -22,9 +22,9 @@ resource "azurerm_data_factory" "phdi_data_factory" {
 }
 locals {
   ingestion-pipeline-config = jsondecode(templatefile("../modules/data_factory/ingestion-pipeline.json", {
-    fhir_converter_url         = var.fhir_converter_url,
-    ingestion_container_url    = var.ingestion_container_url,
-    fhir_server_url            = var.fhir_server_url
+    fhir_converter_url      = var.fhir_converter_url,
+    ingestion_container_url = var.ingestion_container_url,
+    fhir_server_url         = var.fhir_server_url
   }))
 }
 
