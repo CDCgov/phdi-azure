0cb5d136a20093a8f0cc6f5704be142f75c6191c
Brady Fausett at Skylight <brady@skylight.digital>
2022-11-03 15:16:49 -0600

    Update main.tf

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -4,6 +4,7 @@ module "shared" {
   source              = "../modules/shared"
   resource_group_name = var.resource_group_name
   location            = var.location
+  subscription_id     = var.subscription_id
 }
 
 
