245e29ad32100f1fa27e02d0cf4ffc733fa186c3
Nick Clyde <nick@clyde.tech>
2022-11-04 09:10:12 -0700

    Add ADF identity to FHIR server access policy (#17)

--- terraform/implementation/variables.tf
+++ terraform/implementation/variables.tf
@@ -11,10 +11,22 @@ variable "resource_group_name" {
   description = "value of the Azure resource group to deploy to"
 }
 
+variable "smarty_auth_id" {
+  description = "value of the SmartyStreets Auth ID"
+}
+
+variable "smarty_auth_token" {
+  description = "value of the SmartyStreets Auth Token"
+}
+
 variable "fhir_converter_url" {
   description = "URL of the FHIR converter"
 }
 
 variable "ingestion_container_url" {
   description = "URL of the ingestion container"
+}
+
+variable "ingestion_container_identity_id" {
+  description = "object ID of the identity used by the ingestion container"
 }
