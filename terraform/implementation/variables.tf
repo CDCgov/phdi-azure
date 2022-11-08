9522d38a1199e254c8d8051d9a6849c2be3dc17b
DanPaseltiner <dan@skylight.digital>
2022-11-08 11:23:04 -0500

    Add tenant_id as a var in implementation.

--- terraform/implementation/variables.tf
+++ terraform/implementation/variables.tf
@@ -2,6 +2,10 @@ variable "subscription_id" {
   description = "value of the Azure Subscription ID to use"
 }
 
+variable "tenant_id" {
+  description = "value of the Azure Tenant ID to use"
+}
+
 variable "location" {
   description = "value of the Azure location to deploy to"
   default     = "Central US"
