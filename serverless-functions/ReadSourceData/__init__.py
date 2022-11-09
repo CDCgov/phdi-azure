2e4b5c65063289d1260d82b48044ee0b40da304a
DanPaseltiner <dan@skylight.digital>
2022-11-09 09:45:34 -0500

    Handle espace characters properly.

--- serverless-functions/ReadSourceData/__init__.py
+++ serverless-functions/ReadSourceData/__init__.py
@@ -65,6 +65,7 @@ def main(blob: func.InputStream) -> None:
 
     failed_pipeline_executions = {}
     for idx, message in enumerate(messages):
+        message = message.replace("\\", "\\\\")
         pipeline_parameters = {
             "message": message,
             "message_type": message_type,
