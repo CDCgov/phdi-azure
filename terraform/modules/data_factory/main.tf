5332a1bdf727a15f9c07ae27a653cdcb057d742f
Nick Clyde <nick@clyde.tech>
2022-12-15 14:57:33 -0800

    Add environment name to ingestion pipeline name

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -33,7 +33,7 @@ locals {
 }
 
 resource "azurerm_data_factory_pipeline" "phdi_ingestion" {
-  name            = "phdi-ingestion"
+  name            = "phdi-${terraform.workspace}-ingestion"
   data_factory_id = azurerm_data_factory.phdi_data_factory.id
   concurrency     = 10 // Max concurrent instances of the pipeline, between 1 and 50. May need to tune this in the future. 
   parameters = {
