ac95688cc1d36e332e32d141a437617d3ee1937a
George Hager <george@skylight.digital>
2023-03-09 11:40:35 -0500

    add xmlns xsi tag

--- serverless-functions/ReadSourceData/__init__.py
+++ serverless-functions/ReadSourceData/__init__.py
@@ -170,22 +170,16 @@ def get_reportability_response(
     return reportability_response
 
 
+# extract rr fields and insert them into the ecr
 def rr_to_ecr(rr, ecr):
-    # storage_account_url = "https://phdidevphi9d194c64.blob.core.windows.net"
-    # container_name = "source-data"
-    # filename1 = "ecr/CDA_RR.xml"
-    # filename2 = "ecr/CDA_eICR.xml"
-
-    # cred_manager = AzureCredentialManager(resource_location=storage_account_url)
-    # cloud_container_connection = AzureCloudContainerConnection(
-    #     storage_account_url=storage_account_url, cred_manager=cred_manager
-    # )
-    # rr = cloud_container_connection.download_object(
-    #     container_name=container_name, filename=filename1
-    # )
-    # ecr = cloud_container_connection.download_object(
-    #     container_name=container_name, filename=filename2
-    # )
+    # add xmlns:xsi attribute if not there
+    lines = ecr.splitlines()
+    xsi_tag = "xmlns:xsi"
+    if xsi_tag not in lines[0]:
+        lines[0] = lines[0].replace(
+            'xmlns="urn:hl7-org:v3"',
+            'xmlns="urn:hl7-org:v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"')
+        ecr = "\n".join(lines)
 
     rr = etree.fromstring(rr)
     ecr = etree.fromstring(ecr)
@@ -205,15 +199,6 @@ def rr_to_ecr(rr, ecr):
     # Find root-level elements and add them to a list
     for tag in rr_tags:
         rr_elements.append(rr.find(f"./{tag}", namespaces=rr.nsmap))
-
-    # TODO remove this - makes sure rr_elements contains everything we want
-    # for element in rr_elements:
-    #     if (element.attrib):
-    #         print(str(element.tag) + " " + str(element.attrib))
-    #     elif (element.text):
-    #         print(str(element.tag) + " " + str(element.text))
-    #     else:
-    #         print(str(element.tag))
 
     # Find the nested entry element that we need
     entry_tag = "{urn:hl7-org:v3}" + "component/structuredBody/component/section/entry"
@@ -236,27 +221,13 @@ def rr_to_ecr(rr, ecr):
     # Create the section element with root-level elements
     # and entry to insert in the eICR
     if rr_entry is not None:
-        # TODO figure out if we need to make the tag this way
         ecr_section_tag = "{urn:hl7-org:v3}" + "section"
         ecr_section = etree.Element(ecr_section_tag)
-        # ecr_section.set('xmlns', 'urn:hl7-org:v3')  # not sure if we need this
         ecr_section.extend(rr_elements)
         ecr_section.append(rr_entry)
 
     # Append the ecr section into the eCR - puts it at the end
     ecr.append(ecr_section)
-
-    # # TODO remove this - makes sure the section is in the ecr with all elements
-    # section_tag = "{urn:hl7-org:v3}" + "section"
-    # all_sections = ecr.findall(f"./{section_tag}", namespaces=ecr.nsmap)
-    # print(all_sections[0].tag)
-    # print(all_sections[0][0].tag)
-    # print(all_sections[0][1].tag)
-    # print(all_sections[0][2].tag)
-    # print(all_sections[0][3].tag)
-    # print(all_sections[0][4].tag)
-    # print(all_sections[0][5].tag)
-    # print(all_sections[0][6].tag)
 
     ecr = etree.tostring(ecr, encoding='unicode', method='xml')
 
