b0834b80c15507b7ac39e24d7dfe1641b281b2b8
George Hager <george@skylight.digital>
2023-03-16 14:42:33 -0400

    add message parser to terraform

--- terraform/modules/data_factory/main.tf
+++ terraform/modules/data_factory/main.tf
@@ -25,6 +25,7 @@ locals {
   ingestion-pipeline-config = jsondecode(templatefile("../modules/data_factory/ingestion-pipeline.json", {
     fhir_converter_url                      = var.fhir_converter_url,
     ingestion_container_url                 = var.ingestion_container_url,
+    message_parser_url                      = var.message_parser_url, 
     fhir_server_url                         = var.fhir_server_url,
     storage_account_url                     = var.phi_storage_account_endpoint_url,
     fhir_upload_failures_container_name     = var.fhir_upload_failures_container_name,
--- terraform/modules/data_factory/variables.tf
+++ terraform/modules/data_factory/variables.tf
@@ -18,11 +18,10 @@ variable "ingestion_container_url" {
   description = "URL of the ingestion container"
 }
 
-# TODO: Uncomment when message parser is implemented
-# variable "message_parser_url" {
-#  type        = string
-#  description = "URL of the message parser container"
-# }
+variable "message_parser_url" {
+  type        = string
+  description = "URL of the message parser container"
+}
 
 # TODO: Uncomment when validation is implemented
 # variable "validation_container_url" {
