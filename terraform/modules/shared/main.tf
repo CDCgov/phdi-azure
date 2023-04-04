ee2e7c2e293e0fe7621848c86c66a179b870b0da
Nick Clyde <nick@clyde.tech>
2023-04-04 14:56:35 -0700

    Add firewall rule to Postgres to allow access from Azure services (#73)

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -264,6 +264,16 @@ resource "azurerm_postgresql_flexible_server_database" "mpi" {
   charset   = "utf8"
 }
 
+// Allow Azure services to access the database
+// See here: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_firewall_rule
+resource "azurerm_postgresql_firewall_rule" "allow_azure_services" {
+  name                = "allow_azure_services"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_flexible_server.mpi.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
 resource "azurerm_container_app" "container_app" {
   for_each                     = local.images
   name                         = "phdi-${terraform.workspace}-${each.key}"
