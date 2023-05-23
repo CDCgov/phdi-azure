9b4ad2fdd98173ecb751b61f498eb2ac79739ed0
nickbristow <richard.nick.bristow@gmail.com>
2023-05-23 16:05:38 -0400

    test

--- terraform/modules/shared/data.tf
+++ terraform/modules/shared/data.tf
@@ -1,5 +1,5 @@
 data "azurerm_client_config" "current" {}
 
 data "azuread_application" "github_app" {
-  display_name = "github-${var.resource_group_name}-${terraform.workspace}"
+  display_name = "github-${var.resource_group_name}"
 }
