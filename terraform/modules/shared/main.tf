670df9b2b7a7aa24fcf064abd07c314b3cceaf74
Daniel Paseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2023-06-20 15:10:30 -0400

    upgrade to v1.0.7

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -236,7 +236,7 @@ locals {
 
 data "docker_registry_image" "ghcr_data" {
   for_each = local.images
-  name     = "ghcr.io/cdcgov/phdi/${each.key}:v1.0.6"
+  name     = "ghcr.io/cdcgov/phdi/${each.key}:v1.0.7"
 }
 
 resource "docker_image" "ghcr_image" {
