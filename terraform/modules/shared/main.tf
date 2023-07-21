a2f0f3b55ecd21436e97c875ffda61e4e67dd4aa
m-goggins <marcelle.goggins@gmail.com>
2023-07-21 12:29:36 -0700

    bump to verson 1.0.9

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -247,7 +247,7 @@ locals {
 
 data "docker_registry_image" "ghcr_data" {
   for_each = local.images
-  name     = "ghcr.io/cdcgov/phdi/${each.key}:v1.0.7"
+  name     = "ghcr.io/cdcgov/phdi/${each.key}:v1.0.9"
 }
 
 resource "docker_image" "ghcr_image" {
