c9455fabe5befd71f7753146384d6149db5a041e
emmastephenson <emma.stephenson.24@gmail.com>
2023-07-21 11:33:19 -0700

    remove FHIR server resource

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -478,35 +478,35 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 }
 
 ##### FHIR Server #####
-
-resource "azurerm_healthcare_service" "fhir_server" {
-  name                = "${terraform.workspace}fhir${substr(var.client_id, 0, 8)}"
-  location            = "eastus"
-  resource_group_name = var.resource_group_name
-  kind                = "fhir-R4"
-  cosmosdb_throughput = 400
-
-  lifecycle {
-    ignore_changes = [name, tags]
-  }
-
-  tags = {
-    environment = terraform.workspace
-    managed-by  = "terraform"
-  }
-}
-
-resource "azurerm_role_assignment" "gh_sp_fhir_contributor" {
-  scope                = azurerm_healthcare_service.fhir_server.id
-  role_definition_name = "FHIR Data Contributor"
-  principal_id         = var.object_id
-}
-
-resource "azurerm_role_assignment" "pipeline_runner_fhir_contributor" {
-  scope                = azurerm_healthcare_service.fhir_server.id
-  role_definition_name = "FHIR Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.pipeline_runner.principal_id
-}
+#
+#resource "azurerm_healthcare_service" "fhir_server" {
+#  name                = "${terraform.workspace}fhir${substr(var.client_id, 0, 8)}"
+#  location            = "eastus"
+#  resource_group_name = var.resource_group_name
+#  kind                = "fhir-R4"
+#  cosmosdb_throughput = 400
+#
+#  lifecycle {
+#    ignore_changes = [name, tags]
+#  }
+#
+#  tags = {
+#    environment = terraform.workspace
+#    managed-by  = "terraform"
+#  }
+#}
+#
+#resource "azurerm_role_assignment" "gh_sp_fhir_contributor" {
+#  scope                = azurerm_healthcare_service.fhir_server.id
+#  role_definition_name = "FHIR Data Contributor"
+#  principal_id         = var.object_id
+#}
+#
+#resource "azurerm_role_assignment" "pipeline_runner_fhir_contributor" {
+#  scope                = azurerm_healthcare_service.fhir_server.id
+#  role_definition_name = "FHIR Data Contributor"
+#  principal_id         = azurerm_user_assigned_identity.pipeline_runner.principal_id
+#}
 
 ##### User Assigned Identity #####
 
