3f85ed78c6f77425d0f24c6a9885205429f81d51
emmastephenson <emma.stephenson.24@gmail.com>
2023-07-26 13:25:32 -0700

    try again with the name

--- terraform/modules/shared/data.tf
+++ terraform/modules/shared/data.tf
@@ -1,10 +1,10 @@
 data "azurerm_client_config" "current" {}
 
-data "azurerm_kubernetes_cluster" "cluster_data" {
-  name = "phdi-${terraform.workspace}-cluster_data"
-  resource_group_name = var.resource_group_name
-
-  depends_on = [
-    azurerm_kubernetes_cluster.cluster
-  ]
-}
+#data "azurerm_kubernetes_cluster" "cluster_data" {
+#  name = "phdi-${terraform.workspace}-cluster_data"
+#  resource_group_name = var.resource_group_name
+#
+#  depends_on = [
+#    azurerm_kubernetes_cluster.cluster
+#  ]
+#}
