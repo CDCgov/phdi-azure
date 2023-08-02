a98fb5a8265927ec52a848f13e0b810f8c87c89d
nickbristow <richard.nick.bristow@gmail.com>
2023-08-02 16:35:37 -0400

    add location to vnet

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -463,6 +463,7 @@ resource "azurerm_virtual_network" "aks_vnet" {
   name                = "phdi-${terraform.workspace}-vnet"
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
+  location            = var.location
 
   subnet {
     name           = "phdi-${terraform.workspace}-aks_subnet"
