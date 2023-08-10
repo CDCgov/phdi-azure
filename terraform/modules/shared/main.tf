2a0f77edabe40cc91fc7ae692eab36c1a27b3a86
emmastephenson <emma.stephenson.24@gmail.com>
2023-08-10 13:47:07 -0700

    put a little of the provider back

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -533,14 +533,14 @@ data "azurerm_kubernetes_cluster" "credentials" {
   resource_group_name = var.resource_group_name
 }
 
-#provider "helm" {
+provider "helm" {
 #  kubernetes {
 #    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
 #    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
 #    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
 #    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
 #  }
-#}
+}
 #
 #resource "helm_release" "record_linkage" {
 #  name          = "phdi-${terraform.workspace}"
