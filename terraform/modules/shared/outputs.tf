08bcdb37c84e2b56f3f8e90e2f7fd9b1c897dcd7
Daniel Paseltiner <99684231+DanPaseltiner@users.noreply.github.com>
2023-08-16 12:01:17 -0400

    Remove FHIR server (#268)

--- terraform/modules/shared/outputs.tf
+++ terraform/modules/shared/outputs.tf
@@ -6,10 +6,6 @@ output "phi_storage_account_key" {
   value = azurerm_storage_account.phi.primary_access_key
 }
 
-output "fhir_server_name" {
-  value = azurerm_healthcare_service.fhir_server.name
-}
-
 output "pipeline_runner_id" {
   value = azurerm_user_assigned_identity.pipeline_runner.id
 }
