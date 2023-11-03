33e658f266d9fff91b368a8d7cc3ae06176bbde9
Nick Clyde <nick@clyde.tech>
2023-11-03 08:59:40 -0700

    Okay latest_revision_fqdn

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -546,7 +546,7 @@ resource "azurerm_healthcare_fhir_service" "fhir_server" {
   }
 
   cors {
-    allowed_origins     = ["https://${azurerm_container_app.container_app["ingestion"].fqdn}"]
+    allowed_origins     = ["https://${azurerm_container_app.container_app["ingestion"].latest_revision_fqdn}"]
     allowed_headers     = ["*"]
     allowed_methods     = ["GET", "DELETE", "PUT"]
     max_age_in_seconds  = 3600
