fe76e5fac6fc72e375694e151267211ac23d74bf
Nick Clyde <nick@clyde.tech>
2023-11-02 20:18:07 -0700

    Fix location and rg

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -521,8 +521,8 @@ locals {
 
 resource "azurerm_healthcare_workspace" "fhir_server" {
   name                = local.fhir_server_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_healthcare_fhir_service" "fhir_server" {
